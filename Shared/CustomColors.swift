5a106f5b5c248f8fef4e489fca67a27af4ae8c41
rosa <rosatiaragaluh@gmail.com>
2023-02-18 16:20:00 +0700

    init

---
+++ Shared/CustomColors.swift
@@ -0,0 +1,21 @@
+//
+//  CustomColors.swift
+//  oishiramen
+//
+//  Created by T on 18/02/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    public static var black: Color {
+        return Color("black")
+    }
+    public static var orange: Color {
+       return Color(red: 235/255, green: 134/255, blue: 72/255)
+    }
+    public static var gray: Color {
+        return Color(red: 32/255, green: 32/255, blue: 32/255)
+    }
+}
