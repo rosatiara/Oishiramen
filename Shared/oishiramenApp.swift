aa879b7925a23b2dacb382d0521cd81bca20f98a
rosa <rosatiaragaluh@gmail.com>
2023-02-18 16:07:08 +0700

    Initial Commit

---
+++ Shared/oishiramenApp.swift
@@ -0,0 +1,17 @@
+//
+//  oishiramenApp.swift
+//  Shared
+//
+//  Created by T on 18/02/23.
+//
+
+import SwiftUI
+
+@main
+struct oishiramenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
