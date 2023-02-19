80f1c5df0a2c1e0df17fd3ce8a69ad97ecfea04a
rosa <rosatiaragaluh@gmail.com>
2023-02-19 21:36:10 +0700

    add card component

---
+++ Shared/Components/Card.swift
@@ -0,0 +1,35 @@
+//
+//  Card.swift
+//  oishiramen
+//
+//  Created by T on 19/02/23.
+//
+
+import SwiftUI
+
+let ramens = ["arigatou", "chikinkare", "konnichiwa", "midoriro", "niko"]
+
+
+struct Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // VStack
+            // Menu
+                // Ramen image
+                // ramen name
+                // stars
+            // Descriptions
+                // HStack
+                    // heart
+                    // price
+                    // add button
+        
+        
+    }
+}
+
+struct Card_Previews: PreviewProvider {
+    static var previews: some View {
+        Card()
+    }
+}
