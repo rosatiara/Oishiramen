c6c4add288e2b550b7aa825804cb2a5ac8da0ed9
rosa <rosatiaragaluh@gmail.com>
2023-02-20 14:05:24 +0700

    fix: add comma lol

--- Shared/Components/CardView.swift
+++ Shared/Components/CardView.swift
@@ -18,9 +18,9 @@ struct Card {
 struct CardView: View {
     
     let cards = [
-        Card(image: Image("chikinkare"), menu: "Chikinkare", rate: "⭐️⭐️⭐️⭐️", price: "$5.4")
-        Card(image: Image("arigatou"), menu: "Arigatou Ramen", rate: "⭐️⭐️⭐️⭐️⭐️", price: "$8.9")
-        Card(image: Image("midoriro"), menu: "Midoriro Yasai", rate: "⭐️⭐️⭐️⭐️⭐️", price: "$6.3")
+        Card(image: Image("chikinkare"), menu: "Chikinkare", rate: "⭐️⭐️⭐️⭐️", price: "$5.4"),
+        Card(image: Image("arigatou"), menu: "Arigatou Ramen", rate: "⭐️⭐️⭐️⭐️⭐️", price: "$8.9"),
+        Card(image: Image("midoriro"), menu: "Midoriro Yasai", rate: "⭐️⭐️⭐️⭐️⭐️", price: "$6.3"),
         Card(image: Image("niko"), menu: "Niko To Kinoko", rate: "⭐️⭐️⭐️⭐️⭐️", price: "$8.9")
     ]
     
