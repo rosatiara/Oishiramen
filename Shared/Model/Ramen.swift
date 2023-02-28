e292e851a6bd3c06e55669570fb3017eab74f88a
rosa <rosatiaragaluh@gmail.com>
2023-02-28 11:30:26 +0700

    fix ratings

--- Shared/Model/Ramen.swift
+++ Shared/Model/Ramen.swift
@@ -17,9 +17,9 @@ struct Ramen: Identifiable {
 }
 
 let ramens = [
-    Ramen(ramenImage: "chikinkare", ramenName: "Chikinkare", ramenRating: "4.3/5", price: "$5.4"),
-    Ramen(ramenImage: "arigatou", ramenName: "Arigatou Ramen", ramenRating: "4.9/5", price: "$8.9"),
-    Ramen(ramenImage: "midoriro", ramenName: "Midoriro Yasai", ramenRating: "4.86/5", price: "$6.3"),
-    Ramen(ramenImage: "niko", ramenName: "Niko To Kinoko", ramenRating: "4.6/5", price: "$8.9"),
+    Ramen(ramenImage: "chikinkare", ramenName: "Chikinkare", ramenRating: "⭐⭐⭐⭐", price: "$5.4"),
+    Ramen(ramenImage: "arigatou", ramenName: "Arigatou Ramen", ramenRating: "⭐⭐⭐⭐⭐", price: "$8.9"),
+    Ramen(ramenImage: "midoriro", ramenName: "Midoriro Yasai", ramenRating: "⭐⭐⭐⭐⭐", price: "$6.3"),
+    Ramen(ramenImage: "niko", ramenName: "Niko To Kinoko", ramenRating: "⭐⭐⭐⭐⭐", price: "$8.9"),
 
 ]
